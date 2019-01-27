dc58690cc7f76fae77d00eadcb97c658e7bcf24e
maciejmazurek <maciej.mazurek.prv@gmail.com>
2019-01-27 13:48:00 +0100

    Turned off contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,16 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-if (process.env.ENVIRONMENT !== "production") {
+if (process.env.ENVIRONMENT !== 'production') {
   dotenv.config();
 }
 
-const { spaceId, accessToken } = process.env;
+const {spaceId, accessToken} = process.env;
 
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
+    description:
+      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@gatsbyjs',
   },
   plugins: [
@@ -35,15 +36,15 @@ module.exports = {
         display: 'minimal-ui',
       },
     },
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId,
-        accessToken
-      }
-    },
+    // {
+    //   resolve: "gatsby-source-contentful",
+    //   options: {
+    //     spaceId,
+    //     accessToken
+    //   }
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
